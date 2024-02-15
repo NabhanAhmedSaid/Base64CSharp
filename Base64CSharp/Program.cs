43d078f2800b43f176e362cc5227a7fdb61b808a
safcsp dev <dev@test.com>
2024-02-15 12:39:25 +0400

    Add project files.

---
+++ Base64CSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Text;
+
+Console.WriteLine("Welcome to app\n" +
+    "1. encoded message \n" +
+    "2. decoded message \n");
+int input = Int32.Parse(Console.ReadLine());
+
+if (input ==1)
+{
+    Console.WriteLine("Enter your message");
+    var message = Console.ReadLine();
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+    var encodedMessage = Convert.ToBase64String(messageBytes);
+    Console.WriteLine(encodedMessage);
+}
+if (input ==2)
+{
+    Console.WriteLine("Enter the encoded " +
+        "Message");
+    var encodedMessage = Console.ReadLine();
+    var decodedBytes = Convert.FromBase64String(encodedMessage);
+    var originalMessage = Encoding.UTF8.GetString(decodedBytes);
+    Console.WriteLine(originalMessage);
+}
